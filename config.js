4f86cc527d369e736c735bf99760d40e9c5cd5ec
YourName <your.email@example.com>
2025-04-27 15:32:54 +0700

    Updated MongoDB connection string to new cluster

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // in a production environment
 
 module.exports = {
-  MONGODB_URI: 'mongodb+srv://Rettey:Adhu1447@cluster0.qi38xbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  MONGODB_URI: 'mongodb+srv://Rettey:Adhu1447@cluster0.hriuovn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   PORT: process.env.PORT || 5000,
   JWT_SECRET: 'juniorjoy-secret-key',
   JWT_EXPIRES_IN: '24h'
